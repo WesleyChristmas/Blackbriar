ce59d1928dcbe0c77bdecadaf323b8c38ab8e52e
WesleyChristmas <Keen@Napster.local>
2016-11-11 15:06:29 +0300

    Upload project
    two pages, styles, scripts

---
+++ js/angularController/Home.js
@@ -0,0 +1,209 @@
+﻿var app = angular.module('homeApp', []);
+
+app.directive('onFinishRender', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$parent.$last === true) {
+                $timeout(function () {
+                    scope.$emit(attr.onFinishRender);
+                });
+            }
+        }
+	}
+});
+
+app.controller('HomeController', function($scope) {
+	
+	$scope.$on('ngRepeatFinished', function(e){
+		console.log(e);
+		console.log('good to go');
+	});
+	
+	var result = [
+		{
+			IdRecord : 0,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			VIP : false,
+			Hot : true,
+			Food : true,
+			Taxi : true,
+			Music : true,
+			Liked : true,
+		},
+		{
+			IdRecord : 1,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+			Music : true,
+		},
+		{
+			IdRecord : 2,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+			Music : true,
+		},
+		{
+			IdRecord : 3,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+			Music : true,
+		},
+		{
+			IdRecord : 4,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : true,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+			Music : true,
+		},
+		{
+			IdRecord : 5,
+			Photo : 'img/loft.jpg',
+			Title : 'Современный лофт на Чистых прудах',
+			Price : 3000,
+			MaxPeople: 30,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+			Music : true,
+		},
+		{
+			IdRecord : 6,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : true,
+			Food : true,
+			Taxi : true,
+			Music : true,
+		},
+		{
+			IdRecord : 7,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : true,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+		},
+		{
+			IdRecord : 8,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : true,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+		},
+		{
+			IdRecord : 9,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : true,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+		},
+		{
+			IdRecord : 10,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : true,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+		},
+		{
+			IdRecord : 11,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+			Food : true,
+			Taxi : true,
+		},
+		{
+			IdRecord : 12,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+		},
+		{
+			IdRecord : 13,
+			Photo : 'img/loft.jpg',
+			Title : 'Пати-кафе на Черкизовской',
+			Price : 2000,
+			MaxPeople: 50,
+			Hot : true,
+			Liked : false,
+			VIP : false,
+		},
+		{
+			IdRecord : 14,
+			Photo : 'img/loft.jpg',
+			Title : 'Современный лофт на Чистых прудах',
+			Price : 3000,
+			MaxPeople: 30,
+			Hot : true,
+			Liked : false,
+			VIP : true,
+		},
+	];
+    
+	$scope.Result = result;
+	console.log($scope.Result);
+});
---
+++ slider/admin-ajax.php
@@ -0,0 +1,16 @@
+
+<!-- saved from url=(0057)http://demo.stylishthemes.co/diva/wp-admin/admin-ajax.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style id="style-1-cropbar-clipper">/* Copyright 2014 Evernote Corporation. All rights reserved. */
+.en-markup-crop-options {
+    top: 18px !important;
+    left: 50% !important;
+    margin-left: -100px !important;
+    width: 200px !important;
+    border: 2px rgba(255,255,255,.38) solid !important;
+    border-radius: 4px !important;
+}
+
+.en-markup-crop-options div div:first-of-type {
+    margin-left: 0px !important;
+}
+</style></head><body>0</body></html>
