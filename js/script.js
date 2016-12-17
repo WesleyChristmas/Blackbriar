db406220ee4fa7ccf079d8949c704c1b5301a8a0
WesleyChristmas <wesley.keen@icloud.com>
2016-12-18 00:47:53 +0300

    ночной коммит

--- js/script.js
+++ js/script.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
     e.preventDefault();
     $(this).tab('show');
   });
+
+$(window).scroll(function(){
+	        if(document.body.scrollTop > 10)
+	            $('#toppanel').addClass( "main_menu_wrap_scrolled" );
+	        else   
+	            $('#toppanel').removeClass( "main_menu_wrap_scrolled" );
+	    });
+	 
+	    $('a#srolltotop').click(function(){
+	        $('html, body').animate({scrollTop:0}, 100);
+	        return false;
+	    });
+
 });
